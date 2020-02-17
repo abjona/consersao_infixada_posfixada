6fb35204369746776b99a3a4da2432536aa60ac2
Jonathan Ramses Alves Borges <14070643630@muzambinho.edu.br>
2020-02-17 16:45:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [infixada, setInfixada] = useState('');
+
+  function calc(){
+    const pilha = [];
+    infixada.forEach((element, i) => {
+      pilha.push(element[i]);
+    });
+  }
+  function prioridade(char){
+    if(char == '(') return 1;
+    else {
+      if(char == '*' || char == '/') return 2;
+      else{
+        return 3;
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box">
+        <h4 className="title">CONVERSÃO</h4>
+        <h5 className="label">
+            Digite a expressão :
+        </h5>
+        <input className="inp" placeholder="infixada" value={infixada} onChange={event=> setInfixada(event.target.value)}></input>
+
+        <button className="btn" onClick={calc}> CALCULAR </button>
+
+        <div className="result"> </div>
+      </div>
     </div>
   );
 }
